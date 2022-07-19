e51d0b1d4e32b41c7ff20057c9dec2522c12cd84
Tektronica <ryalho@gmail.com>
2022-07-19 11:45:52 -0700

    fixed prop names in about modal

--- nextjs-frontend/components/modal/ModalAbout.js
+++ nextjs-frontend/components/modal/ModalAbout.js
@@ -19,7 +19,7 @@ const ModalAbout = ({ isOpen, setIsOpen }) => {
                 <Dialog.Panel className="border w-full max-w-sm rounded bg-white">
 
                     {/* <!-- Modal header --> */}
-                    <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                    <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
 
                         <Dialog.Title className='text-xl font-semibold text-gray-900 dark:text-white'>
                             About
@@ -30,7 +30,7 @@ const ModalAbout = ({ isOpen, setIsOpen }) => {
                             type="button"
                             className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal"
                         >
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </button>
 
                     </div>
@@ -39,7 +39,7 @@ const ModalAbout = ({ isOpen, setIsOpen }) => {
                         Version: v1.0
                     </Dialog.Description>
 
-                    <div class="p-4 w-full max-w-lg">
+                    <div className="p-4 w-full max-w-lg">
                         <p className='text-base leading-relaxed text-cyan-800 dark:text-gray-400'>
                             “Calpi is a react frontend hosted on a Raspberry Pi 4 for the purpose of conducting calibration and measurement. The interface is able to review test reports, manually toggle gpio pins, and send individual commands to instruments.”
                         </p>
